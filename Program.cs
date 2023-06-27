308265dfc20bc870e5ce7727290e939ef4a8f2e4
Michal Jakusz <jakusz.it@gmail.com>
2023-06-27 21:30:08 +0200

    Drugi Commit

--- Program.cs
+++ Program.cs
@@ -2,32 +2,5 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        ProduktSpozywczy produkt = new ProduktSpozywczy();
-        produkt.Nazwa = "Jogurt";
-        produkt.CenaNetto = 2.50m;
-        produkt.KrajPochodzenia = "Polska";
-        produkt.Kalorie = 150;
-        produkt.Alergeny = new Dictionary<string, bool>()
-            {
-                { "Gluten", false },
-                { "Mleko", true }
-            };
 
-        Console.WriteLine("Produkt: " + produkt.Nazwa);
-        Console.WriteLine("Cena Netto: " + produkt.CenaNetto);
-        Console.WriteLine("Cena Brutto: " + produkt.CenaBrutto);
-        Console.WriteLine("Kategoria VAT: " + produkt.KategoriaVAT);
-        Console.WriteLine("Kraj Pochodzenia: " + produkt.KrajPochodzenia);
-        Console.WriteLine("Kalorie: " + produkt.Kalorie);
-        Console.WriteLine("Alergeny:");
-        foreach (var alergen in produkt.Alergeny)
-        {
-            Console.WriteLine(alergen.Key + ": " + (alergen.Value ? "Tak" : "Nie"));
-        }
-
-        Console.ReadKey();
-    }
-}
 }
